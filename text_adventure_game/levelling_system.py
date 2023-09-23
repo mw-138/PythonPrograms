0cc95f23eac828451cecd1ef4239f68739468cbe
mw138 <mattheww138@gmail.com>
2023-09-24 00:11:39 +0100

    Added levelling system

---
+++ text_adventure_game/levelling_system.py
@@ -0,0 +1,47 @@
+import math
+
+
+class LevellingSystem:
+    def __init__(self, max_level, constant, power):
+        self.max_level = max_level
+        self.constant = constant
+        self.power = power
+        self.experience = 0
+
+    def get_current_level(self):
+        return 1 + math.floor(self.constant * math.sqrt(self.experience))
+
+    def get_previous_level(self):
+        level = self.get_current_level()
+        return 0 if level <= 0 else level - 1
+
+    def get_next_level(self):
+        return self.max_level if self.has_reached_max_level() else self.get_current_level() + 1
+
+    def has_reached_max_level(self):
+        return self.get_current_level() >= self.max_level
+
+    def get_experience_for_level(self, level):
+        return math.ceil(math.pow(level / self.constant, self.power))
+
+    def get_experience_for_next_level(self):
+        return self.get_experience_for_level(self.get_next_level())
+
+    def get_experience_for_previous_level(self):
+        return self.get_experience_for_level(self.get_previous_level())
+
+    def give_experience(self, amount):
+        if self.has_reached_max_level():
+            return
+        self.experience += amount
+
+    def remove_experience(self, amount):
+        if self.experience <= 0:
+            return
+        self.experience -= amount
+
+    def get_experience_progress(self):
+        return math.floor(self.experience / self.get_experience_for_next_level() * 100)
+
+    def get_remaining_experience(self):
+        return self.get_experience_for_next_level() - self.experience
