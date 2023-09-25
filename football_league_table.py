97c9e038360d1741a31a9c478032f43acaf9e1d3
mw138 <mattheww138@gmail.com>
2023-09-25 19:33:48 +0100

    Added team rating system and goal weights

--- football_league_table.py
+++ football_league_table.py
@@ -1,11 +1,12 @@
 import random
-
 import helpers
 
 
 class Team:
-    def __init__(self, label):
+    def __init__(self, label, rating):
         self.label = label
+        self.rating = rating
+        self.max_rating = 10
         self.games_won = 0
         self.games_drawn = 0
         self.games_lost = 0
@@ -13,7 +14,7 @@ class Team:
         self.goals_against = 0
 
     def get_games_played(self):
-        return self.games_lost + self.games_drawn + self.games_won
+        return self.games_won + self.games_drawn + self.games_lost
 
     def get_goal_difference(self):
         return self.goals_for - self.goals_against
@@ -24,6 +25,12 @@ class Team:
     def sort_condition(self):
         return self.get_points(), self.get_goal_difference()
 
+    def get_win_chance(self):
+        return round(self.rating / self.max_rating * 100)
+
+    def clamp_rating(self):
+        helpers.clamp(self.rating, 0, self.max_rating)
+
 
 class Fixture:
     def __init__(self, home_team, away_team):
@@ -31,23 +38,67 @@ class Fixture:
         self.away_team = away_team
 
     def determine_winner(self):
-        home_score = helpers.random_inclusive(0, 3)
-        away_score = helpers.random_inclusive(0, 3)
+        winner_population = [self.home_team, self.away_team]
+        winner_weights = (self.home_team.rating, self.away_team.rating)
+        winner = random.choices(population=winner_population, weights=winner_weights, k=1)[0]
+        did_home_win = self.home_team == winner
+
+        goal_weights = {
+            0: 80,
+            1: 90,
+            2: 60,
+            3: 50,
+            4: 20,
+            5: 5,
+            6: 0.5,
+            7: 0.5,
+            8: 0.5,
+            9: 0.5,
+            10: 0.5
+        }
+
+        rating_change_min = 0.01
+        rating_change_max = 0.05
+        max_goals_population = []
+        max_goals_weights = []
+
+        for key in goal_weights:
+            max_goals_population.append(key)
+            max_goals_weights.append(goal_weights[key])
+
+        max_goals_to_score = random.choices(population=max_goals_population, weights=max_goals_weights, k=1)[0]
+
+        if did_home_win:
+            away_score = helpers.random_inclusive(0, max_goals_to_score)
+            home_score = away_score + helpers.random_inclusive(0, max_goals_to_score) + 1
+            self.home_team.rating += random.uniform(rating_change_min, rating_change_max)
+            self.away_team.rating -= random.uniform(rating_change_min, rating_change_max)
+            self.home_team.clamp_rating()
+            self.away_team.clamp_rating()
+        else:
+            home_score = helpers.random_inclusive(0, max_goals_to_score)
+            away_score = home_score + helpers.random_inclusive(0, max_goals_to_score) + 1
+            self.home_team.rating -= random.uniform(rating_change_min, rating_change_max)
+            self.away_team.rating += random.uniform(rating_change_min, rating_change_max)
+            self.home_team.clamp_rating()
+            self.away_team.clamp_rating()
 
         self.home_team.goals_for += home_score
         self.away_team.goals_for += away_score
         self.home_team.goals_against += away_score
         self.away_team.goals_against += home_score
 
-        if home_score == away_score:
-            self.home_team.games_drawn += 1
-            self.away_team.games_drawn += 1
-        elif home_score > away_score:
+        if did_home_win:
             self.home_team.games_won += 1
             self.away_team.games_lost += 1
-        elif home_score < away_score:
+        elif not did_home_win:
             self.home_team.games_lost += 1
             self.away_team.games_won += 1
+        else:
+            self.home_team.games_drawn += 1
+            self.away_team.games_drawn += 1
+
+        print(f"{self.home_team.label} ({home_score}) vs ({away_score}) {self.away_team.label}")
 
 
 class League:
@@ -91,7 +142,7 @@ class League:
         for team in self.teams:
             to_print.append(
                 f"{pos} | "
-                f"{team.label} | "
+                f"{team.label.upper()} | "
                 f"GP: {team.get_games_played()} | "
                 f"W: {team.games_won} | "
                 f"D: {team.games_drawn} | "
@@ -99,7 +150,8 @@ class League:
                 f"GF: {team.goals_for} | "
                 f"GA: {team.goals_against} | "
                 f"GD: {team.get_goal_difference()} | "
-                f"PTS: {team.get_points()}"
+                f"PTS: {team.get_points()} | "
+                f"WIN CHANCE: {team.get_win_chance()}%"
             )
             pos += 1
 
@@ -120,24 +172,24 @@ class FootballLeagueTable:
 
 
 premier_league = League("Premier League", [
-    Team("Arsenal"),
-    Team("Aston Villa"),
-    Team("Bournemouth"),
-    Team("Brentford"),
-    Team("Brighton And Hover Albion"),
-    Team("Burnley"),
-    Team("Chelsea"),
-    Team("Crystal Palace"),
-    Team("Everton"),
-    Team("Fulham"),
-    Team("Liverpool"),
-    Team("Luton Town"),
-    Team("Manchester City"),
-    Team("Manchester United"),
-    Team("Newcastle United"),
-    Team("Nottingham Forest"),
-    Team("Sheffield United"),
-    Team("Tottenham Hotspur"),
-    Team("West Ham United"),
-    Team("Wolverhampton Wanderers")
+    Team("Arsenal", 8),
+    Team("Aston Villa", 6),
+    Team("Bournemouth", 5),
+    Team("Brentford", 5),
+    Team("Brighton And Hover Albion", 7),
+    Team("Burnley", 3),
+    Team("Chelsea", 4),
+    Team("Crystal Palace", 4),
+    Team("Everton", 3),
+    Team("Fulham", 4),
+    Team("Liverpool", 7),
+    Team("Luton Town", 2),
+    Team("Manchester City", 9),
+    Team("Manchester United", 7),
+    Team("Newcastle United", 7),
+    Team("Nottingham Forest", 5),
+    Team("Sheffield United", 2),
+    Team("Tottenham Hotspur", 7),
+    Team("West Ham United", 6),
+    Team("Wolverhampton Wanderers", 5)
 ], [1, 5, 7, 20])
