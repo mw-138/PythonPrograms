4e83913648fd5df3f1ce86c0ee1f2e0735d13495
mw138 <mattheww138@gmail.com>
2023-09-30 17:15:22 +0100

    Added random item rarities and healing items

--- text_adventure_game/inventory_item.py
+++ text_adventure_game/inventory_item.py
@@ -1,4 +1,8 @@
+import random
 from enum import Enum
+
+import helpers
+from text_adventure_game.levelling_system import LevellingSystem
 
 
 class ItemRarity(Enum):
@@ -9,7 +13,7 @@ class ItemRarity(Enum):
     Legendary = 5
 
 
-__rarity_weights = {
+rarity_weights = {
     ItemRarity.Common: 80,
     ItemRarity.Uncommon: 70,
     ItemRarity.Rare: 50,
@@ -19,8 +23,6 @@ __rarity_weights = {
 
 
 # TODO: Make rarity weight chances increase based on player level
-# TODO: Maybe add levelling to item
-# TODO: Remove rarity from constructor, add weighted chances to rarity and give item random rarity on construction
 class InventoryItem:
     def __init__(self, label, max_stack, rarity, sell_price, chance):
         self.label = label
@@ -30,16 +32,29 @@ class InventoryItem:
         self.chance = chance
         self.can_be_equipped = False
 
+    def _generate_random_rarity(self):
+        population, weights = helpers.dictionary_to_random_choices(rarity_weights)
+        random_rarity = random.choices(weights=weights, population=population, k=1)[0]
+        return random_rarity
+
 
 class WeaponInventoryItem(InventoryItem):
-    def __init__(self, label, max_stack, rarity, sell_price, chance, damage):
-        super().__init__(label, max_stack, rarity, sell_price, chance)
+    def __init__(self, label, max_stack, max_level, sell_price, chance, damage):
+        super().__init__(label, max_stack, self._generate_random_rarity(), sell_price, chance)
         self.damage = damage
         self.can_be_equipped = True
+        self.levelling = LevellingSystem(max_level, 0.07, 2)
 
 
 class ArmorInventoryItem(InventoryItem):
-    def __init__(self, label, max_stack, rarity, sell_price, chance, protection):
-        super().__init__(label, max_stack, rarity, sell_price, chance)
+    def __init__(self, label, max_stack, max_level, sell_price, chance, protection):
+        super().__init__(label, max_stack, self._generate_random_rarity(), sell_price, chance)
         self.protection = protection
         self.can_be_equipped = True
+        self.levelling = LevellingSystem(max_level, 0.07, 2)
+
+
+class HealingInventoryItem(InventoryItem):
+    def __init__(self, label, max_stack, rarity, sell_price, chance, heal_amount):
+        super().__init__(label, max_stack, rarity, sell_price, chance)
+        self.heal_amount = heal_amount
--- text_adventure_game/item_list.py
+++ text_adventure_game/item_list.py
@@ -1,9 +1,63 @@
-from text_adventure_game.inventory_item import InventoryItem, ItemRarity, WeaponInventoryItem, ArmorInventoryItem
+from text_adventure_game.inventory_item import InventoryItem, ItemRarity, WeaponInventoryItem, ArmorInventoryItem, \
+    HealingInventoryItem
 
 __max_item_stack = 99999
+__max_item_level = 100
 
 item_list = {
-    "old_boot": InventoryItem("Old Boot", __max_item_stack, ItemRarity.Common, 1, 75),
-    "longsword": WeaponInventoryItem("Longsword", 1, ItemRarity.Legendary, 1000, 20, 50),
-    "helmet": ArmorInventoryItem("Helmet", 1, ItemRarity.Rare, 750, 10, 30)
+    "old_boot": InventoryItem(
+        "Old Boot",
+        __max_item_stack,
+        ItemRarity.Common,
+        100,
+        1
+    ),
+    "longsword": WeaponInventoryItem(
+        "Longsword",
+        1,
+        __max_item_level,
+        1000,
+        20,
+        50
+    ),
+    "helmet": ArmorInventoryItem(
+        "Helmet",
+        1,
+        __max_item_level,
+        750,
+        10,
+        30
+    ),
+    "lesser_healing_potion": HealingInventoryItem(
+        "Lesser Healing Potion",
+        __max_item_stack,
+        ItemRarity.Common,
+        10,
+        40,
+        5
+    ),
+    "greater_healing_potion": HealingInventoryItem(
+        "Greater Healing Potion",
+        __max_item_stack,
+        ItemRarity.Rare,
+        10,
+        25,
+        15
+    ),
+    "superior_healing_potion": HealingInventoryItem(
+        "Superior Healing Potion",
+        __max_item_stack,
+        ItemRarity.Epic,
+        10,
+        20,
+        30
+    ),
+    "pot_of_healing": HealingInventoryItem(
+        "Pot of Healing",
+        __max_item_stack,
+        ItemRarity.Legendary,
+        10,
+        5,
+        100
+    )
 }
